b8e9ac5d6ded4067f5113b598bf941d91bcf9291
Ultimate 365 <maidul365@gmail.com>
2025-04-12 20:55:32 +0530

    Slide Images Added

--- src/app/photoUpload/page.jsx
+++ src/app/photoUpload/page.jsx
@@ -1,6 +1,4 @@
 "use client";
-import axios from "axios";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import DataTable, { createTheme } from "react-data-table-component";
